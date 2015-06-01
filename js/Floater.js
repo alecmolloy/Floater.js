8cae88c0d20609cd86a82a5bf16b51416a868f61
Alec Molloy <alec@kano.me>
2015-06-01 14:25:02 -0400

    updated names for arrays

--- js/Floater.js
+++ js/Floater.js
@@ -4,57 +4,57 @@ function Floater(fieldX, fieldY, anchors, lines, segments, relationships) {
     this.fieldX = fieldX; // int
     this.fieldY = fieldY; // int
     this.anchors = anchors || 3; // int
-    this.lines = lines || ['01', '12']; // Array of strings: line anchor pairs
-    this.segments = segments || [20]; // Array of ints: segment numbers per line
-    this.relationships = relationships || ['01']; // Array of strings: related lines
+    lines = lines || ['01', '12']; // Array of strings: line anchor pairs
+    segments = segments || [20]; // Array of ints: segment numbers per line
+    relationships = relationships || ['01']; // Array of strings: related lines
 
-    this.anchor = [];
-    this.line = [];
-    this.relationship = [];
-    this.connector = [[]];
+    this.anchorArray = [];
+    this.lineArray = [];
+    this.relationshipArray = [];
+    this.connectorArray = [[]];
 
     // Anchor
     for (var i = 0; i < this.anchors; i++) {
         var anchorX = Math.round(Math.random() * this.fieldX);
         var anchorY = Math.round(Math.random() * this.fieldY);
-        this.anchor[i] = new this.Anchor(anchorX, anchorY, i);
+        this.anchorArray[i] = new this.Anchor(anchorX, anchorY, i);
     }
 
 
     // Line
-    for (var i = 0; i < this.lines.length; i++) {
-        var anchor1 = this.anchor[this.lines[i].slice(0, 1)];
-        var anchor2 = this.anchor[this.lines[i].slice(1, 2)];
-        this.line[i] = new this.Line(anchor1, anchor2, this.segments[i]);
+    for (var i = 0; i < lines.length; i++) {
+        var anchor1 = this.anchorArray[lines[i].slice(0, 1)];
+        var anchor2 = this.anchorArray[lines[i].slice(1, 2)];
+        this.lineArray[i] = new this.Line(anchor1, anchor2, segments[i]);
     }
 
     // Relationships
-    for (var i = 0; i < this.relationships.length; i++) {
-        var line1 = this.line[this.relationships[i].slice(0, 1).charCodeAt() - 48];
-        var line2 = this.line[this.relationships[i].slice(1, 2).charCodeAt() - 48];
-        this.relationship[i] = [line1, line2];
+    for (var i = 0; i < relationships.length; i++) {
+        var line1 = this.lineArray[relationships[i].slice(0, 1).charCodeAt() - 48];
+        var line2 = this.lineArray[relationships[i].slice(1, 2).charCodeAt() - 48];
+        this.relationshipArray[i] = [line1, line2];
 
-        for (var j = 0; j < this.relationships.length; j++) {
-            var x1 = this.line[i].anchor1.x;
-            var y1 = this.line[i].anchor1.y;
-            var x2 = this.line[i].anchor2.x;
-            var y2 = this.line[i].anchor2.y;
+        for (var j = 0; j < relationships.length; j++) {
+            var x1 = this.lineArray[i].anchor1.x;
+            var y1 = this.lineArray[i].anchor1.y;
+            var x2 = this.lineArray[i].anchor2.x;
+            var y2 = this.lineArray[i].anchor2.y;
 
-            var connectorX = ((x2 - x1) / this.segments[i]) * j + x1;
-            var connectorY = ((y2 - y1) / this.segments[i]) * j + y1;
+            var connectorX = ((x2 - x1) / segments[i]) * j + x1;
+            var connectorY = ((y2 - y1) / segments[i]) * j + y1;
 
             var connector = new this.Connector(connectorX, connectorY);
-            this.connector[i][j] = connector;
+            this.connectorArray[i][j] = connector;
         }
     }
     console.log('Anchor array: ');
-    console.log(this.anchor);
+    console.log(this.anchorArray);
     console.log('Line array: ');
-    console.log(this.line);
+    console.log(this.lineArray);
     console.log("Relationship: ");
-    console.log(this.relationship);
+    console.log(this.relationshipArray);
     console.log("Connector: ");
-    console.log(this.connector);
+    console.log(this.connectorArray);
 }
 
 Floater.prototype.Anchor = function (x, y, anchorNum) {
